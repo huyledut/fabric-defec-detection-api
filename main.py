00db98cbfd9c5b78810929b66bca9eb4fafc8813
huyledut <huyledut@gmail..com>
2023-05-08 16:56:54 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, File
+from util.services import get_yolov5, get_image_from_bytes
+from starlette.responses import Response
+from io import BytesIO
+from PIL import Image
+import json
+from fastapi.middleware.cors import CORSMiddleware
+
+
+model = get_yolov5()
+
+app = FastAPI(
+    title="Custom YOLOV5 Machine Learning API",
+    description="""Obtain object value out of image
+                    and return image and json result""",
+    version="0.0.1",
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/notify/v1/health')
+def get_health():
+    return dict(msg='OK')
+
+
+@app.post("/object-to-json")
+async def detect_return_json_result(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file, max_size=640)
+    results = model(input_image, size=640)
+    detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
+    detect_res = json.loads(detect_res)
+    return {"result": detect_res}
+
+
+@app.post("/object-to-img")
+async def detect_return_base64_img(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file, max_size=640)
+    results = model(input_image)
+    results.ims # array of original images (as np array) passed to model for inference
+    results.render()  # updates results.ims with boxes and labels
+    for im in results.ims:
+        buffered = BytesIO()
+        im_base64 = Image.fromarray(im)
+        im_base64.save(buffered, format="JPEG")
+    return Response(content=buffered.getvalue(), media_type="image/jpeg")
---
+++ util/services.py
@@ -0,0 +1,23 @@
+import torch
+from PIL import Image
+import io
+
+
+def get_yolov5():
+    torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = torch.hub.load('./yolov5',model= 'custom', path='./models/fabric_defect_4.pt', source= 'local')
+    model.conf = 0.5
+    return model
+
+
+def get_image_from_bytes(binary_image, max_size=1024):
+    input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+    width, height = input_image.size
+    resize_factor = min(max_size / width, max_size / height)
+    resized_image = input_image.resize(
+        (
+            int(input_image.width * resize_factor),
+            int(input_image.height * resize_factor),
+        )
+    )
+    return resized_image
