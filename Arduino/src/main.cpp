62fdc2c39977abb80c54bf66249f364647b8eda0
huyledut <huyledut@gmail..com>
2023-05-17 21:24:52 +0700

    communication rasp

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 
-int step = 4;
-int dir = 5;
-int ena = 8;
+int step = 5;
+int dir = 4;
+int ena = 6;
+
 int sensor = A0;
 int value;
+
+String data;
 
 void setup()
 {
@@ -22,13 +25,14 @@ void setup()
 
 void loop()
 {
-
   value = analogRead(sensor);
   Serial.print("Value read from sensor:");
   Serial.println(value);
 
-  if (value < 150)
+  if (value > 350)
   {
+    delay(1000);
+
     digitalWrite(dir, HIGH);
     for (int x = 0; x < 1000; x++)
     {
@@ -37,17 +41,56 @@ void loop()
       digitalWrite(step, LOW);
       delayMicroseconds(500);
     }
-
-    delay(1000);
-
-    digitalWrite(dir, LOW);
-    for (int x = 0; x < 1000; x++)
+    Serial.println("A");
+    while (true)
     {
-      digitalWrite(step, HIGH);
-      delayMicroseconds(500);
-      digitalWrite(step, LOW);
-      delayMicroseconds(500);
+      if (Serial.available() > 0)
+      {
+        data = Serial.readString();
+        if (data == "B")
+        {
+          digitalWrite(dir, LOW);
+          for (int x = 0; x < 1000; x++)
+          {
+            digitalWrite(step, HIGH);
+            delayMicroseconds(500);
+            digitalWrite(step, LOW);
+            delayMicroseconds(500);
+          }
+          break;
+        }
+      }
     }
+
     delay(1000);
+
+    // if (Serial.available() > 0)
+    // {
+    //   data = Serial.readString();
+    //   if (data == "B")
+    //   {
+    //     digitalWrite(dir, LOW);
+    //     for (int x = 0; x < 1000; x++)
+    //     {
+    //       digitalWrite(step, HIGH);
+    //       delayMicroseconds(500);
+    //       digitalWrite(step, LOW);
+    //       delayMicroseconds(500);
+    //     }
+    //     delay(1000);
+    //   }
+    // }
+    // else
+    // {
+    //   digitalWrite(dir, LOW);
+    //   for (int x = 0; x < 1000; x++)
+    //   {
+    //     digitalWrite(step, HIGH);
+    //     delayMicroseconds(1500);
+    //     digitalWrite(step, LOW);
+    //     delayMicroseconds(1500);
+    //   }
+    //   delay(1000);
+    // }
   }
 }
